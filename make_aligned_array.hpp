849713d21b1f828f26d622bd2fa769f9612a9114
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2016-07-26 17:43:46 -0500

    Revampe make_aligned_array to support alignments which are not powers of 2.

--- make_aligned_array.hpp
+++ make_aligned_array.hpp
@@ -26,6 +26,34 @@ inline constexpr bool is_power_of_2(T t)
     return bool((t != 0) && !(t & (t - 1)));
 }
 
+template <std::size_t Alignment>
+void* align_ptr(void* ptr, std::ptrdiff_t size, std::ptrdiff_t space) noexcept
+{
+    if (0 == Alignment)
+    {
+        if (size > space)
+            return nullptr;
+        else
+            return ptr;
+    }
+
+    auto const start = reinterpret_cast<std::uintptr_t>(ptr);
+
+    auto aligned = start; 
+    while (0 != (aligned % Alignment))
+        ++aligned;
+
+    auto const diff = aligned - start;
+
+    if ((size + diff) > space)
+        return nullptr;
+    else
+    {
+        space -= diff;
+        return ptr = reinterpret_cast<void*>(aligned);
+    }
+}
+
 template <typename T>
 struct free_deleter
 {
@@ -35,14 +63,17 @@ struct free_deleter
     }
 };
 
+template <typename T, std::uint64_t Alignment = 64>
+struct aligned_array_ptr;
+
 template <
     typename T
   , std::uint64_t Alignment = 64
-  , typename = typename std::enable_if<is_power_of_2(Alignment)>::type
     >
-inline std::unique_ptr<T[], free_deleter<T> > make_aligned_array(
-    std::ptrdiff_t size
-    ) noexcept
+inline typename std::enable_if<
+    is_power_of_2(Alignment)
+  , aligned_array_ptr<T, Alignment>
+>::type make_aligned_array(std::ptrdiff_t size) noexcept
 {
     static_assert( true == std::is_pod<T>::value
                  , "T must be POD");
@@ -63,10 +94,105 @@ inline std::unique_ptr<T[], free_deleter<T> > make_aligned_array(
 
     std::memset(p, 0, size * sizeof(T));
 
-    free_deleter<T> const d;
-
-    return std::unique_ptr<T[], free_deleter<T> >(reinterpret_cast<T*>(p), d);
+    return aligned_array_ptr<T, Alignment>(
+        reinterpret_cast<T*>(p), reinterpret_cast<T*>(p)
+    );
 }
+
+template <
+    typename T
+  , std::uint64_t Alignment = 64
+    >
+inline typename std::enable_if<
+    !is_power_of_2(Alignment)
+  , aligned_array_ptr<T, Alignment>
+>::type make_aligned_array(std::ptrdiff_t size) noexcept
+{
+    static_assert( true == std::is_pod<T>::value
+                 , "T must be POD");
+    static_assert( 0 == (Alignment % sizeof(void*))
+                 , "Alignment must be a multiple of sizeof(void*)");
+
+    auto const space = (size * sizeof(T)) + Alignment;
+
+    void* p = ::malloc(space);
+    assert(p);
+
+    void* ap = align_ptr<Alignment>(p, size * sizeof(T), space);
+    assert(ap);
+
+    #if defined(__INTEL_COMPILER) && __INTEL_COMPILER >= 1700
+        // Prior versions of ICPC won't accept a non-type template argument
+        // as a parameter to __assume_aligned().
+        __assume_aligned(ap, Alignment);
+    #endif
+
+    std::memset(ap, 0, size * sizeof(T));
+
+    return aligned_array_ptr<T, Alignment>(
+        reinterpret_cast<T*>(p), reinterpret_cast<T*>(ap)
+    );
+}
+
+template <typename T, std::uint64_t Alignment>
+struct aligned_array_ptr
+{
+    static_assert( true == std::is_pod<T>::value
+                 , "T must be POD");
+
+    using size_type = std::ptrdiff_t;
+    using value_type = T;
+    using pointer = typename std::add_pointer<T>::type;
+    using reference = typename std::add_lvalue_reference<T>::type;
+
+  private:
+    std::unique_ptr<T[], free_deleter<T> > true_ptr_;
+    T* aligned_ptr_;
+
+    explicit aligned_array_ptr(T* true_ptr, T* aligned_array_ptr)
+      : true_ptr_(true_ptr, free_deleter<T>()), aligned_ptr_(aligned_array_ptr)
+    {}
+
+    friend aligned_array_ptr make_aligned_array<T, Alignment>(
+        std::ptrdiff_t size
+        ) noexcept;
+
+  public:
+    aligned_array_ptr()
+      : true_ptr_(nullptr, free_deleter<T>()), aligned_ptr_(nullptr)
+    {}
+
+    aligned_array_ptr(aligned_array_ptr&& other)
+      : true_ptr_(std::move(other.true_ptr_))
+      , aligned_ptr_(std::move(other.aligned_ptr_))
+    {}
+
+    aligned_array_ptr& operator=(aligned_array_ptr&& other) noexcept
+    {
+        std::swap(true_ptr_, other.true_ptr_);
+        std::swap(aligned_ptr_, other.aligned_ptr_);
+    }
+
+    reference operator[](size_type i) const noexcept
+    {
+        return aligned_ptr_[i];
+    }
+
+    reference operator*() const noexcept
+    {
+        return *aligned_ptr_;
+    }
+
+    pointer operator->() const noexcept
+    {
+        return aligned_ptr_;
+    }
+
+    pointer get() const noexcept
+    {
+        return aligned_ptr_;
+    }
+};
 
 #endif // CXX_CC1C4B22_3289_48FD_AE9B_BB88B3928D01
 
