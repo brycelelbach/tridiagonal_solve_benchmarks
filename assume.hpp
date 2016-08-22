7e500ef493d53a07802672a2051a87357156fc9c
Bryce Adelstein-Lelbach aka wash <balelbach@lbl.gov>
2016-08-22 01:09:46 -0700

    Add TSB_ASSUME_ALIGNED_TO_TYPE() macro which works around the ICPC bug disallowing alignment assumptions based on template-parameter-dependent constexpr information.

--- assume.hpp
+++ assume.hpp
@@ -11,6 +11,40 @@
 #include <cstdint>
 #include <cassert>
 
+#include "always_inline.hpp"
+
+namespace tsb
+{
+
+template <typename T>
+void assume_aligned_to_type(T* ptr) noexcept TSB_ALWAYS_INLINE;
+
+// Prior versions of ICPC won't accept a non-type template argument or a
+// constexpr expression that is dependent on a template argument as a parameter
+// to __assume_aligned(). As a workaround, we can fully specialize
+// assume_aligned_to_type() for all the types we care about.
+#if !defined(__INTEL_COMPILER) || (__INTEL_COMPILER >= 1700)
+    template <typename T>
+    void assume_aligned_to_type(T* ptr) noexcept
+    {
+        __assume_aligned(ptr, sizeof(T));
+    }
+#else
+    template <>
+    void assume_aligned_to_type(float* ptr) noexcept
+    {
+        __assume_aligned(ptr, sizeof(float));
+    }
+
+    template <>
+    void assume_aligned_to_type(double* ptr) noexcept
+    {
+        __assume_aligned(ptr, sizeof(double));
+    }
+#endif
+
+} // tsb
+
 #if defined(NDEBUG)
     #define TSB_ASSUME(expr)                                                    \
         {                                                                       \
@@ -20,6 +54,11 @@
     #define TSB_ASSUME_ALIGNED(p, alignment)                                    \
         {                                                                       \
             __assume_aligned(p, alignment);                                     \
+        }                                                                       \
+        /**/
+    #define TSB_ASSUME_ALIGNED_TO_TYPE(p)                                       \
+        {                                                                       \
+            ::tsb::assume_aligned_to_type(p);                                   \
         }                                                                       \
         /**/
 #else
@@ -33,6 +72,12 @@
         {                                                                       \
             assert(0 == (std::uintptr_t(p) % alignment));                       \
             __assume_aligned(p, alignment);                                     \
+        }                                                                       \
+        /**/
+    #define TSB_ASSUME_ALIGNED_TO_TYPE(p)                                       \
+        {                                                                       \
+            assert(0 == (std::uintptr_t(p) % sizeof(decltype(*p))));            \
+            ::tsb::assume_aligned_to_type(p);                                   \
         }                                                                       \
         /**/
 #endif
